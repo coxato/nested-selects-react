ffe35c9246204c93a8c16e837a58a468a9e823e6
Carlos Martinez <edu6jas@gmail.com>
2020-09-28 18:26:12 -0400

    Updates

---
+++ precache-manifest.133bc7e19449c3fc15d16818e23b94ce.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c78b5e3ec8bbfed6907cbcc3678d3053",
+    "url": "./index.html"
+  },
+  {
+    "revision": "23a699785df1bf958e77",
+    "url": "./static/css/2.03923625.chunk.css"
+  },
+  {
+    "revision": "4aea6658d74f69799511",
+    "url": "./static/css/main.c7939a64.chunk.css"
+  },
+  {
+    "revision": "23a699785df1bf958e77",
+    "url": "./static/js/2.d0905094.chunk.js"
+  },
+  {
+    "revision": "01dc5313d5caa14c0070a16a31752588",
+    "url": "./static/js/2.d0905094.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4aea6658d74f69799511",
+    "url": "./static/js/main.c003c2ef.chunk.js"
+  },
+  {
+    "revision": "730920da50d06c9385ed",
+    "url": "./static/js/runtime-main.60189eab.js"
+  }
+]);
